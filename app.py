1c36814e2f2967b68bb6a4052feb03753d30a06e
shvmiera <shamierabatrisyiabilqis@gmail.com>
2025-05-26 17:26:51 +0900

    Save local changes before pull

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from werkzeug.utils import secure_filename # for secure name
 from datetime import datetime #datetime
 
 client = MongoClient("mongodb://localhost:27017/") # connect on the "localhost" host and port 27017
-db = client["chef"] # use/create "webapp" database
+db = client["webapp"] # use/create "webapp" database
 recipe_col = db.recipe # use/create "recipe" collection
 user_col = db['user'] # use/create "user" collection
 
@@ -46,6 +46,7 @@ def login():
             if bcrypt.checkpw(password.encode('utf-8'), user_data['password']):
                 # correct login: create session
                 session['user_email'] = email
+                session['username'] = user_data['username']
                 flash("Logged in successfully!")
                 return redirect(url_for('index')) 
             else:
@@ -102,8 +103,19 @@ def register():
 
 @app.route('/myrecipes')
 def myrecipes():
-    recipes = list(recipe_col.find())  # Fetch all recipes as a list of dicts from MongoDB
-    return render_template('myrecipes.html', recipes=recipes)
+    username = session.get('username')
+    if not username:
+        flash("Please log in to view your recipes.")
+        return redirect(url_for('login'))
+
+    my_recipes = list(recipe_col.find({'prepared_by': username}))
+
+    saved_recipe_ids = session.get('saved_recipe_ids', [])
+    saved_recipes = list(recipe_col.find({'_id': {'$in': [ObjectId(id) for id in saved_recipe_ids]}}))
+
+    return render_template('myrecipes.html', 
+                           saved_recipes=saved_recipes, 
+                           my_recipes=my_recipes)
 
 @app.route('/recipe/title/<recipe_title>')
 def recipedetails(recipe_title):
@@ -117,6 +129,12 @@ def recipedetails(recipe_title):
 @app.route('/addrecipe')
 def addrecipe():
     return render_template('addrecipe.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()  # remove all session data
+    flash("You’ve been logged out successfully.")
+    return redirect(url_for('login'))
 
 @app.route('/test')
 def test():
